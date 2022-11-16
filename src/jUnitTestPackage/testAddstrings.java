39c9b337d7381b9f8309ec2dc98e339fc6d1e6a0
Jaya Saini <Jaya Saini@10.12.39.132>
2022-11-16 17:32:21 +0530

    testing

---
+++ src/jUnitTestPackage/testAddstrings.java
@@ -0,0 +1,16 @@
+package jUnitTestPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testAddstrings {
+
+	@Test
+	public void test() {
+		jUnitFunctions junitstring= new jUnitFunctions();
+		String result = junitstring.addStrings("capstone", "project");
+		assertEquals("capstoneproject",result);
+	}
+
+}
